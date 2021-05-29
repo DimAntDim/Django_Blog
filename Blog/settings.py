0196a4bc0dd115d7a33ac4ac8d78656139bab33f
Your NameDimAntDim <dimantdim1985@gmail.com>
2021-05-28 21:35:51 -0500

    Templates

--- Blog/settings.py
+++ Blog/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -52,10 +53,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'Blog.urls'
 
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = ''
+STATICFILES_DIR = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- blog_app/models.py
+++ blog_app/models.py
@@ -1,9 +1,19 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
 
 class Post(models.Model):
+    # default manager
+    objects = models.Manager()
+    #  Custom manager returning only published Post's
+    published = PublishedManager()
+
     STATUS_CHOICES = (
         ('draft', 'Draft'), 
         ('published', 'Published'),
@@ -25,3 +35,6 @@ class Post(models.Model):
 
         def __str__(self):
             return self.title
+    
+    def get_absolute_url(self):
+        return reverse('blog: post_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slug])

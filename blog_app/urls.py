a83ce1a0b5a0ab5e3a1469da5a5e60e6e587f226
Dimitar Dimitrov <66394357+DimAntDim@users.noreply.github.com>
2021-05-30 20:54:44 -0500

    Delete blog_app directory

--- blog_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('<int:year>/<int:monht>/<int:day>/<slug:post>', views.post_detail, name='post_detail'),
-]
